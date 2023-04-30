d8fde2de72e11f4d0104b1b42873d430fb9cd28d
Your Name <you@example.com>
2023-04-30 18:50:13 +0300

    some courses mailing changes 2

--- fit_bot/telegram_bot/handlers/courses_interaction/info.py
+++ fit_bot/telegram_bot/handlers/courses_interaction/info.py
@@ -15,39 +15,38 @@ def get_courses(message: Message):
     # Итерация по оплаченным пользователям
     for user in paid_users:
         # Находим соответствующую категорию контента на основе характеристик пользователя
-        matching_category = Категории.objects.get(
-            пол=user.пол,
-            цель=user.цель,
-            место=user.место,
-            уровень=user.уровень
-        )
+        try:
+            matching_category = Категории.objects.get(
+                пол=user.пол,
+                цель=user.цель,
+                место=user.место,
+                уровень=user.уровень
+            )
 
-        # Вычисляем номер дня на основе оплаченного дня
-        delta_days = (timezone.now().date() - user.paid_day).days
-        current_day = delta_days
-        if current_day != 0:
-            # Получаем контент для соответствующей категории и дня
-            try:
-                daily_contents = DailyContent.objects.filter(category=matching_category, day=current_day, sequence_number__gte=3)
-
-                if daily_contents:
-                    # Отправляем контент пользователю через Telegram Bot API
-                    for content in daily_contents:
-                        updated_caption = content.caption.replace("calories", str(user.calories)).replace("name",
-                                                                                                          user.full_name)
-                        if content.content_type == 'V':
-                            bot.send_video(chat_id=user.user, video=content.video_file_id, caption=updated_caption)
-                        elif content.content_type == 'T':
-                            bot.send_message(chat_id=user.user, text=updated_caption)
-                        elif content.content_type == 'P':
-                            bot.send_photo(chat_id=user.user, photo=content.photo_file_id, caption=updated_caption)
-                        elif content.content_type == 'G':
-                            bot.send_document(chat_id=user.user, document=content.gif_file_id, caption=updated_caption)
-                else:
-                    bot.send_message(chat_id=user.user, text='Кажется, что на сегодня для вас нет тренировок! следуйте инструкциям')
-            except:
-                bot.send_message(chat_id=user.user,
-                                 text='Кажется, что на сегодня для вас нет тренировок! следуйте инструкциям')
+            # Вычисляем номер дня на основе оплаченного дня
+            delta_days = (timezone.now().date() - user.paid_day).days
+            current_day = delta_days
+            if current_day != 0:
+                # Получаем контент для соответствующей категории и дня
+                    daily_contents = DailyContent.objects.filter(category=matching_category, day=current_day, sequence_number__gte=3)
+                    if daily_contents:
+                        # Отправляем контент пользователю через Telegram Bot API
+                        for content in daily_contents:
+                            updated_caption = content.caption.replace("calories", str(user.calories)).replace("name",
+                                                                                                              user.full_name)
+                            if content.content_type == 'V':
+                                bot.send_video(chat_id=user.user, video=content.video_file_id, caption=updated_caption)
+                            elif content.content_type == 'T':
+                                bot.send_message(chat_id=user.user, text=updated_caption)
+                            elif content.content_type == 'P':
+                                bot.send_photo(chat_id=user.user, photo=content.photo_file_id, caption=updated_caption)
+                            elif content.content_type == 'G':
+                                bot.send_document(chat_id=user.user, document=content.gif_file_id, caption=updated_caption)
+                    else:
+                        bot.send_message(chat_id=user.user, text='Кажется, что на сегодня для вас нет тренировок! следуйте инструкциям')
+        except:
+            bot.send_message(chat_id=user.user,
+                             text='Кажется, что на сегодня для вас нет тренировок! следуйте инструкциям')
 
         else:
             bot.send_message(user.user, 'Курс начнется со следующего дня! '
