961475b26a99106a43c4e0df2938ffc6ecd411ef
Your Name <you@example.com>
2023-06-26 16:36:32 +0300

    minor

--- fit_bot/telegram_bot/warm_up_bot/handlers/mailings.py
+++ fit_bot/telegram_bot/warm_up_bot/handlers/mailings.py
@@ -40,69 +40,71 @@ def test_state(call: CallbackQuery):
 def check_unfinished_users():
     conn, cursor = get_cursor()
     now = datetime.now()
+    try:
+        cursor.execute('SELECT user_id, last_interaction_time, notified FROM Users WHERE phone IS NULL')
+        for user_id, last_interaction_time, notified in cursor.fetchall():
+            if last_interaction_time is not None:
+                last_interaction_time = datetime.strptime(last_interaction_time, '%Y-%m-%d %H:%M:%S')
+                if (now - last_interaction_time > timedelta(minutes=3) and notified == 0) or \
+                    (now - last_interaction_time > timedelta(minutes=15) and notified == 1):
+                    markup = InlineKeyboardMarkup()
+                    button1 = InlineKeyboardButton(text='Продолжить!', callback_data='continueafteremiind')
+                    markup.add(button1)
+                    if now - last_interaction_time > timedelta(minutes=3) and notified == 0:
+                        bot.send_photo(chat_id=user_id,
+                                       photo='AgACAgIAAxkBAAICV2SIgoutZLFnVveTMW74rf'
+                                             '1Cq1RwAAIUxzEbHdtBSMUWHDZ--IkjAQADAgADeQADLwQ',
+                                       caption="*Упс, кажется вы отвлеклись...*\n\n"
+                                               "Чтобы продолжить, нажмите на кнопку снизу👇",
+                                       reply_markup=markup, parse_mode='Markdown')
+                    elif now - last_interaction_time > timedelta(minutes=15) and notified == 1:
+                        bot.send_photo(chat_id=user_id,
+                                       photo='AgACAgIAAxkBAAICb2SIjMrh9wd3YwSuMvcBI0_bfW-MAA'
+                                             'JwxzEbHdtBSAPISViAFsjUAQADAgADeQADLwQ',
+                                       caption="📞 *Вы здесь?* Не стала вас отвлекать, подумала что вы заняты...\n\n"
+                                               "Вы не заполнили форму до конца, а заявки Ибрат получит "
+                                               "*после прохождения всех этапов.*\n\nЧтобы заполнить форму до конца, "
+                                               "нажмите на кнопку 👇",
+                                       reply_markup=markup, parse_mode='Markdown')
+                    cursor.execute('UPDATE Users SET last_interaction_time = ?, '
+                                   'notified = ? WHERE user_id = ?',
+                                   [now.strftime('%Y-%m-%d %H:%M:%S'), notified + 1, user_id])
 
-    cursor.execute('SELECT user_id, last_interaction_time, notified FROM Users WHERE phone IS NULL')
-    for user_id, last_interaction_time, notified in cursor.fetchall():
-        if last_interaction_time is not None:
-            last_interaction_time = datetime.strptime(last_interaction_time, '%Y-%m-%d %H:%M:%S')
-            if (now - last_interaction_time > timedelta(minutes=3) and notified == 0) or \
-                (now - last_interaction_time > timedelta(minutes=15) and notified == 1):
-                markup = InlineKeyboardMarkup()
-                button1 = InlineKeyboardButton(text='Продолжить!', callback_data='continueafteremiind')
-                markup.add(button1)
-                if now - last_interaction_time > timedelta(minutes=3) and notified == 0:
-                    bot.send_photo(chat_id=user_id,
-                                   photo='AgACAgIAAxkBAAICV2SIgoutZLFnVveTMW74rf'
-                                         '1Cq1RwAAIUxzEbHdtBSMUWHDZ--IkjAQADAgADeQADLwQ',
-                                   caption="*Упс, кажется вы отвлеклись...*\n\n"
-                                           "Чтобы продолжить, нажмите на кнопку снизу👇",
-                                   reply_markup=markup, parse_mode='Markdown')
-                elif now - last_interaction_time > timedelta(minutes=15) and notified == 1:
-                    bot.send_photo(chat_id=user_id,
-                                   photo='AgACAgIAAxkBAAICb2SIjMrh9wd3YwSuMvcBI0_bfW-MAA'
-                                         'JwxzEbHdtBSAPISViAFsjUAQADAgADeQADLwQ',
-                                   caption="📞 *Вы здесь?* Не стала вас отвлекать, подумала что вы заняты...\n\n"
-                                           "Вы не заполнили форму до конца, а заявки Ибрат получит "
-                                           "*после прохождения всех этапов.*\n\nЧтобы заполнить форму до конца, "
-                                           "нажмите на кнопку 👇",
-                                   reply_markup=markup, parse_mode='Markdown')
-                cursor.execute('UPDATE Users SET last_interaction_time = ?, '
-                               'notified = ? WHERE user_id = ?',
-                               [now.strftime('%Y-%m-%d %H:%M:%S'), notified + 1, user_id])
-
-    cursor.execute('SELECT user_id, last_interaction_time, notified FROM Users WHERE phone IS NOT NULL')
-    for user_id, last_interaction_time, notified in cursor.fetchall():
-        if last_interaction_time is not None:
-            last_interaction_time = datetime.strptime(last_interaction_time, '%Y-%m-%d %H:%M:%S')
-            if (now - last_interaction_time > timedelta(minutes=3) and notified == 0) or \
-                (now - last_interaction_time > timedelta(minutes=15) and notified == 1):
-                markup = InlineKeyboardMarkup()
-                button1 = InlineKeyboardButton(text='Продолжить!', callback_data='continueafteremiind')
-                markup.add(button1)
-                if now - last_interaction_time > timedelta(minutes=3) and notified == 0:
-                    bot.send_photo(chat_id=user_id,
-                                   photo='AgACAgIAAxkBAAICV2SIgoutZLFnVveTM'
-                                         'W74rf1Cq1RwAAIUxzEbHdtBSMUWHDZ--IkjAQADAgADeQADLwQ',
-                                   caption="*Упс, кажется вы отвлеклись...*\n\n"
-                                           "Чтобы продолжить, нажмите на кнопку снизу👇",
-                                   reply_markup=markup, parse_mode='Markdown')
-                elif now - last_interaction_time > timedelta(minutes=15) and notified == 1:
-                    cursor.execute('SELECT username, name, phone FROM Users WHERE user_id = ?', [user_id,])
-                    user_data = cursor.fetchone()
-                    print(user_data)
-                    '''('Grasmurr', 'хелоу', '79670282821')
-                    (None, 'хелоу', '79774563501')'''
-                    username = user_data[0] if user_data[0] is not None else "Отсутствует"
-                    text = f"🧾 Босс! Пользователь не дошел до конца:\n\n" \
-                           f"*Имя:* {user_data[1]}\n" \
-                           f"*Телефон:* +{user_data[2]}\n" \
-                           f"*Username:* @{username}" \
-                           f"\n\nРада была помочь!\nС любовью, Лиза❣️"
-                    bot.send_message(chat_id=58790442, text=text, parse_mode='Markdown')
-                cursor.execute('UPDATE Users SET last_interaction_time = ?, '
-                               'notified = ? WHERE user_id = ?',
-                               [now.strftime('%Y-%m-%d %H:%M:%S'), notified + 1, user_id])
-    conn.commit()
+        cursor.execute('SELECT user_id, last_interaction_time, notified FROM Users WHERE phone IS NOT NULL')
+        for user_id, last_interaction_time, notified in cursor.fetchall():
+            if last_interaction_time is not None:
+                last_interaction_time = datetime.strptime(last_interaction_time, '%Y-%m-%d %H:%M:%S')
+                if (now - last_interaction_time > timedelta(minutes=3) and notified == 0) or \
+                    (now - last_interaction_time > timedelta(minutes=15) and notified == 1):
+                    markup = InlineKeyboardMarkup()
+                    button1 = InlineKeyboardButton(text='Продолжить!', callback_data='continueafteremiind')
+                    markup.add(button1)
+                    if now - last_interaction_time > timedelta(minutes=3) and notified == 0:
+                        bot.send_photo(chat_id=user_id,
+                                       photo='AgACAgIAAxkBAAICV2SIgoutZLFnVveTM'
+                                             'W74rf1Cq1RwAAIUxzEbHdtBSMUWHDZ--IkjAQADAgADeQADLwQ',
+                                       caption="*Упс, кажется вы отвлеклись...*\n\n"
+                                               "Чтобы продолжить, нажмите на кнопку снизу👇",
+                                       reply_markup=markup, parse_mode='Markdown')
+                    elif now - last_interaction_time > timedelta(minutes=15) and notified == 1:
+                        cursor.execute('SELECT username, name, phone FROM Users WHERE user_id = ?', [user_id,])
+                        user_data = cursor.fetchone()
+                        print(user_data)
+                        '''('Grasmurr', 'хелоу', '79670282821')
+                        (None, 'хелоу', '79774563501')'''
+                        username = user_data[0] if user_data[0] is not None else "Отсутствует"
+                        text = f"🧾 Босс! Пользователь не дошел до конца:\n\n" \
+                               f"*Имя:* {user_data[1]}\n" \
+                               f"*Телефон:* +{user_data[2]}\n" \
+                               f"*Username:* @{username}" \
+                               f"\n\nРада была помочь!\nС любовью, Лиза❣️"
+                        bot.send_message(chat_id=58790442, text=text, parse_mode='Markdown')
+                    cursor.execute('UPDATE Users SET last_interaction_time = ?, '
+                                   'notified = ? WHERE user_id = ?',
+                                   [now.strftime('%Y-%m-%d %H:%M:%S'), notified + 1, user_id])
+        conn.commit()
+    except Exception as E:
+        bot.send_message(305378717, f'Ошибка: {E}')
 
 
 # def check_phoneless_users():
