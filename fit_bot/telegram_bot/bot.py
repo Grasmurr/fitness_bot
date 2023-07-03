f7ee47c4d3e0c265150d8c74b681b5ea2839f6bb
Your Name <you@example.com>
2023-07-03 14:09:14 +0300

    minor

--- fit_bot/telegram_bot/bot.py
+++ fit_bot/telegram_bot/bot.py
@@ -19,8 +19,8 @@ bot.add_custom_filter(IsAdminFilter(bot))
 
 def start_bot():
     bot.send_message(305378717, f'Бот запущен! Можете нажать /start')
-    # bot.infinity_polling()
-    bot.infinity_polling(restart_on_change=True)
+    bot.infinity_polling()
+    # bot.infinity_polling(restart_on_change=True)
     bot.send_message(305378717, f'Бот Выключен!')
 
     # while True:
