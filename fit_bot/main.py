99163b6797965331d36ecdd90eb71d273d95d8f7
Your Name <you@example.com>
2023-07-03 14:02:55 +0300

    minor

--- fit_bot/main.py
+++ fit_bot/main.py
@@ -6,8 +6,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fit_bot.settings')
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 django.setup()
 
-from .telegram_bot.bot import start_bot
-from .telegram_bot.tasks import scheduler_thread
+from telegram_bot.bot import start_bot
+from telegram_bot.tasks import scheduler_thread
 
 
 if __name__ == '__main__':
